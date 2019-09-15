f21ea17bddb6bc920b968de8191e5830a3ff993f
wuYin <wuYinPost@gmail.com>
2019-09-15 18:20:42 +0800

    删除排序链表中的重复元素

---
+++ linked-list/83_remove_duplicates_from_sorted_list.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "../utils/util.h"
+
+// 从第二个节点开始遍历，若重复则删除
+struct ListNode *deleteDuplicates(struct ListNode *head) {
+	if (head == NULL)
+		return NULL;
+
+	// head 不会丢失
+	struct ListNode *pre = head;
+	struct ListNode *cur = head->next;
+
+	while (cur != NULL) {
+		if (pre->val == cur->val)
+			pre->next = cur->next; // 重复则删除 cur
+		else
+			pre = cur;
+
+		cur = cur->next;
+	}
+
+	return head;
+}
+
+
+int main() {
+	int nums1[] = {1, 1, 2};
+	struct ListNode *l1 = genList(nums1, 3);
+	prList(deleteDuplicates(l1));
+}
