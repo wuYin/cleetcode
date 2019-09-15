8037368ea5d6ab5634f94fd83b6c8850cf24b94c
wuYin <wuYinPost@gmail.com>
2019-09-15 18:20:42 +0800

    移除链表元素

---
+++ linked-list/203_remove_linked_list_elements.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../utils/util.h"
+
+
+// 注意头结点可能会丢失
+struct ListNode *removeElements(struct ListNode *head, int val) {
+	struct ListNode *dummy = (struct ListNode *) malloc(sizeof(struct ListNode));
+	dummy->next = head;
+
+	struct ListNode *prev, *cur;
+	prev = dummy, cur = head;
+
+	while (cur != NULL) {
+		if (cur->val == val)
+			prev->next = cur->next; // 删除 cur
+		else
+			prev = prev->next;
+
+		cur = cur->next;
+	}
+
+	return dummy->next;
+}
+
+
+int main() {
+	int nums1[] = {1, 1, 2, 3};
+	struct ListNode *l1 = genList(nums1, 4);
+	prList(removeElements(l1, 1)); // 2->3->NULL
+}
