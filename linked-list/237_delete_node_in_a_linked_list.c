05893a97f56c3cf6b8ec77fb24d8387e2a4b245b
wuYin <wuYinPost@gmail.com>
2019-09-15 18:20:42 +0800

    删除链表中的节点

---
+++ linked-list/237_delete_node_in_a_linked_list.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "../utils/util.h"
+
+void deleteNode(struct ListNode *node) {
+	if (node == NULL || node->next == NULL) // node 已知非尾结点
+		return;
+
+	// 巧妙地通过复制下一节点的值实现自删除
+	node->val = node->next->val;
+	node->next = node->next->next;
+}
+
+int main() {
+	int nums1[] = {1, 2, 3, 4, 5};
+	struct ListNode *l1 = genList(nums1, 5);
+	deleteNode(l1->next->next);
+	prList(l1); // 1->2->4->5->NULL
+}
