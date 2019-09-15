acfb26ba1988ab58eb40859fd7e54e86a5f00d49
wuYin <wuYinPost@gmail.com>
2019-09-15 18:20:43 +0800

    链表的中间结点

---
+++ linked-list/876_middle_of_the_linked_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "../utils/util.h"
+
+// 快慢双指针
+struct ListNode *middleNode(struct ListNode *head) {
+	struct ListNode *slow = head, *fast = head;
+
+	while (fast != NULL && fast->next != NULL) {
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return slow;
+}
+
+
+int main() {
+	int nums1[] = {1, 2, 3, 4, 5};
+	struct ListNode *l1 = genList(nums1, 5);
+	prList(middleNode(l1)); // 3->4->5->NULL
+}
